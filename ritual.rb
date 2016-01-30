6851c148a7067f461ad3f62025becfaf2ae72ca2
Michael Rubino <msrubino@gmail.com>
2016-01-30 02:51:55 -0500

    db connectivity

--- ritual.rb
+++ ritual.rb
@@ -1,18 +1,6 @@
 require 'sinatra'
 require 'sinatra/activerecord'
-
-configure :development do
-  db = URI.parse(ENV['DATABASE_URL'] || 'postgres:///localhost/ritualdb')
-
-  ActiveRecord::Base.establish_connection(
-    :adapter  => db.scheme == 'postgres' ? 'postgresql' : db.scheme,
-    :host     => db.host,
-    :username => db.user,
-    :password => db.password,
-    :database => db.path[1..-1],
-    :encoding => 'utf8'
-  )
-end
+require './config/environments'
 
 class Player < ActiveRecord::Base
 end
